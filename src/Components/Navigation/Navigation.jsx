1d0cb9817e520ba68ae3b0bef193650590228e8f
JohnPLeg <kniimare@gmail.com>
2025-07-17 19:48:24 -0400

    added a nav bar

---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import styles from './Navigation.module.css'
+
+function Navigation() {
+  return (
+    <nav className={styles.navbar}>
+      <Link to="/" className={styles.navLink}>Home</Link>
+    </nav>
+  );
+}
+
+
+
+export default Navigation;
