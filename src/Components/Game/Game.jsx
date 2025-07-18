f79171a324b95d2f5cbbc40f56672284b9c43747
JohnPLeg <kniimare@gmail.com>
2025-07-18 09:22:36 -0400

    added point counter

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,4 +1,5 @@
 import deal from "../../Game Logic/dealing";
+import { cards } from "../../Game Logic/cards";
 import { useState, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import styles from "./Game.module.css";
@@ -6,6 +7,8 @@ import styles from "./Game.module.css";
 function Game () {
     const location = useLocation();
     const firstRender = useRef(true);
+    const [points, setPoints] = useState(0);
+    const [playersTurn, setPlayersTurn] = useState(true);
     const [dealerHand, setDealerHand] = useState([]);
     const [hand, setHand] = useState([]);
 
@@ -62,37 +65,85 @@ function Game () {
         const newCard = deal(0, 51);
         setHand(prev => [...prev, newCard]);
 
-        const newDealerCard = deal(0, 51);
         setTimeout(() => {
-            setDealerHand(prev => [...prev, newDealerCard])
+            setPlayersTurn(false);
+
+            setTimeout(() => {
+                const newDealerCard = deal(0, 51);
+                setDealerHand(prev => [...prev, newDealerCard])
+
+                setTimeout(() => {
+                    setPlayersTurn(true);
+                }, 500);
+            }, 1000);
         }, 500);
+
     }
 
+    useEffect(() => {
+        setPoints(calcPlayerPoints());
+    }, [hand])
+
+    function calcPlayerPoints() {
+        let sum = 0;
+        let aces = 0;
+
+        for (let i = 0; i < hand.length; i++) {
+            const card = hand[i];
+            const rank = card.split("_of_")[0];
+
+            if (["jack", "queen", "king"].includes(rank)) {
+                sum += 10;
+            } else if (rank === "ace") {
+                sum += 11;
+                aces += 1;
+            } else {
+                sum += parseInt(rank);
+            }
+        }
+
+        while (sum > 21 && aces > 0) {
+            sum -= 10;
+            aces -= 1;
+        }
+
+        return sum;
+    }
+
+
+    
+
     return (
-        <div className={styles.mainContainer}>
-            <div className={styles.container}>
-                <div className={styles.dealerContainer}>
-                    {(dealerHand.length == 2) ? (
-                        <>
-                            <img src={`/${dealerHand[0]}`} alt="dealers face card" />
-                            <img src="/back_of_card.png" alt="back of a playing card" />
-                        </>
-                    ) : (
-                        <>
-                            {dealerHand.map((card, index) => ( <img key={index} src={`/${card}`}/> ))}
-                        </>
-                    )}
-                </div>
-                <div className={styles.playerContainer}>
-                    {hand.map((card, index) => ( <img key={index} src={`/${card}`}/> ))}
-                </div>
-                <div className={styles.btnContainer}>
-                    <button className={styles.hit} onClick={hit}>Hit</button>
-                    <button className={styles.stand}>Stand</button>
-                    {canSplit()}
+        <>
+            <h1 className={styles.turn}>{(playersTurn) ? "Player" : "Dealer" }'s Turn</h1>
+            <div className={styles.mainContainer}>
+                <div className={styles.container}>
+                    <div className={styles.dealerContainer}>
+                        {(dealerHand.length == 2) ? (
+                            <>
+                                <img src={`/${dealerHand[0]}`} alt="dealers face card" />
+                                <img src="/back_of_card.png" alt="back of a playing card" />
+                            </>
+                        ) : (
+                            <>
+                                {dealerHand.map((card, index) => ( <img key={index} src={`/${card}`}/> ))}
+                            </>
+                        )}
+                    </div>
+                    <div className={styles.playerContainer}>
+                        <h1 className={styles.counter}>{points}</h1>
+                        <div className={styles.cards}>
+                            {hand.map((card, index) => ( <img key={index} src={`/${card}`}/> ))}
+                        </div>
+                    </div>
+                    <div className={styles.btnContainer}>
+                        <button className={styles.hit} onClick={hit}>Hit</button>
+                        <button className={styles.stand}>Stand</button>
+                        {canSplit()}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- src/Game Logic/cards.js
+++ src/Game Logic/cards.js
@@ -55,3 +55,65 @@ export const cards = [
   "king_of_spades.png",
   "ace_of_spades.png"
 ];
+
+export const values = {
+  // Clubs
+  "2_of_clubs.png": 2,
+  "3_of_clubs.png": 3,
+  "4_of_clubs.png": 4,
+  "5_of_clubs.png": 5,
+  "6_of_clubs.png": 6,
+  "7_of_clubs.png": 7,
+  "8_of_clubs.png": 8,
+  "9_of_clubs.png": 9,
+  "10_of_clubs.png": 10,
+  "jack_of_clubs.png": 10,
+  "queen_of_clubs.png": 10,
+  "king_of_clubs.png": 10,
+  "ace_of_clubs.png": 11,
+
+  // Diamonds
+  "2_of_diamonds.png": 2,
+  "3_of_diamonds.png": 3,
+  "4_of_diamonds.png": 4,
+  "5_of_diamonds.png": 5,
+  "6_of_diamonds.png": 6,
+  "7_of_diamonds.png": 7,
+  "8_of_diamonds.png": 8,
+  "9_of_diamonds.png": 9,
+  "10_of_diamonds.png": 10,
+  "jack_of_diamonds.png": 10,
+  "queen_of_diamonds.png": 10,
+  "king_of_diamonds.png": 10,
+  "ace_of_diamonds.png": 11,
+
+  // Hearts
+  "2_of_hearts.png": 2,
+  "3_of_hearts.png": 3,
+  "4_of_hearts.png": 4,
+  "5_of_hearts.png": 5,
+  "6_of_hearts.png": 6,
+  "7_of_hearts.png": 7,
+  "8_of_hearts.png": 8,
+  "9_of_hearts.png": 9,
+  "10_of_hearts.png": 10,
+  "jack_of_hearts.png": 10,
+  "queen_of_hearts.png": 10,
+  "king_of_hearts.png": 10,
+  "ace_of_hearts.png": 11,
+
+  // Spades
+  "2_of_spades.png": 2,
+  "3_of_spades.png": 3,
+  "4_of_spades.png": 4,
+  "5_of_spades.png": 5,
+  "6_of_spades.png": 6,
+  "7_of_spades.png": 7,
+  "8_of_spades.png": 8,
+  "9_of_spades.png": 9,
+  "10_of_spades.png": 10,
+  "jack_of_spades.png": 10,
+  "queen_of_spades.png": 10,
+  "king_of_spades.png": 10,
+  "ace_of_spades.png": 11
+};
